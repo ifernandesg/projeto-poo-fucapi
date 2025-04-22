979edf0966d523ab8596d8ced7e29e5b41ab7644
Igor <eng.igorfg@gmail.com>
2025-04-22 09:57:06 -0400

    Initial commmit

---
+++ src/main/java/com/example/primeiroprojeto/controllers/TestController.java
@@ -0,0 +1,16 @@
+package com.example.primeiroprojeto.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @RequestMapping("/primeiro-projeto")
+    
+    public String Test() {
+        return "Meu primeiro projeto spring, aluno Igor Fernandes Gonçalves";
+    }
+}
